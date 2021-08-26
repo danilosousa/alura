8abe9f899dc523ec5949edf0572d7eb6064d69e8
Danilo <danilo.sousa.ext@dasa.com.br>
2021-08-25 22:44:21 -0300

    ...

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,17 @@
+// declara as variaveis
 const burger = document.getElementById('burger')
 const menu = document.querySelector('.menu')
 
+// adiciona um evento de click no botão
 burger.addEventListener("click", () => {
+    
     burger.classList.toggle('active')
     menu.classList.toggle('active')
+    
     if(menu.classList.contains('active')){
         menu.style.maxHeight = menu.scrollHeight + "px";
     } else {
       menu.removeAttribute("style");  
     }
+
 })
